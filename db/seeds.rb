063947fffdcfb72f07471e6eee493c1ae81a52e8
Deanna Norman <deannaleenorman@msn.com>
2020-12-21 12:38:59 -0800

    Commit 9: Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: protagonist },
+	{ name: "Trinity", description: computer hacker },
+	{ name: "Morpheus", description: captain },
+	{ name: "Agent Smith", description: AI program },
 ])
