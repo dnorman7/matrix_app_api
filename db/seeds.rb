05300ea3e65b4470437cbac0f6817184c0c7a289
Deanna Norman <deannaleenorman@msn.com>
2021-01-08 12:51:40 -0800

    updates seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: protagonist },
-	{ name: "Trinity", description: computer hacker },
-	{ name: "Morpheus", description: captain },
-	{ name: "Agent Smith", description: AI program },
+	{ name: "Neo", description: 'Neo (born as Thomas A. Anderson, also known as The One, an anagram for Neo) is a fictional character and the protagonist of the Matrix franchise.' },
+	{ name: "Trinity", description: 'Trinity is a fictional character in the Matrix franchise.[1] She is portrayed by Carrie-Anne Moss in the films. ' },
+	{ name: "Morpheus", description: 'Morpheus  is a fictional character in The Matrix franchise. He is portrayed by Laurence Fishburne in the films.' },
+	{ name: "Agent Smith", description: 'Agent Smith is a fictional character and the main antagonist of The Matrix trilogy.' },
 ])
